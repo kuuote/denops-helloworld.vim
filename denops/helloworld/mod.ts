486dc4698e270572aade8f59cbcb24c81790edfc
kuuote <znmxodq1@gmail.com>
2021-02-19 17:41:26 +0000

    ほげぇ

--- denops/helloworld/mod.ts
+++ denops/helloworld/mod.ts
@@ -4,35 +4,15 @@ import { Denops } from "https://deno.land/x/denops@v0.5/denops.ts";
 Denops.start(async function (denops: Denops): Promise<void> {
   // Register dispatcher
   denops.extendDispatcher({
-    async echo(text: unknown): Promise<unknown> {
-      if (typeof text !== "string") {
-        throw new Error(
-          `'text' in 'echo()' of ${denops.name} must be a string`,
-        );
-      }
-      return await Promise.resolve(text);
-    },
-
-    async say(app: unknown): Promise<void> {
-      if (typeof app !== "string") {
-        throw new Error(`'app' in 'say()' of ${denops.name} must be a string`);
-      }
-      const name = await denops.call("input", ["Your name: "]);
-      const version = await denops.eval("v:version");
-      await denops.command("redraw");
-      await denops.command(
-        `echomsg 'Hello ${name}. Your are using ${app} in Vim/Neovim ${version}'`,
+    async hoge(hoge: unknown): Promise<void> {
+      await denops.call(
+        "setline", [1, Array.from(Array(100), (_, i) => i.toString())]
       );
     },
   });
 
-  // Add command
   await denops.command(
-    `command! DenopsHelloWorldEcho echo denops#request("${denops.name}", "echo", ["This is hello world message"])`,
-  );
-
-  await denops.command(
-    `command! DenopsHelloWorldSay echo denops#notify("${denops.name}", "say", ["Denops"])`,
+    `command! Hoge call denops#notify("${denops.name}", "hoge", ["a"])`
   );
 
   console.log("denops-helloworld.vim has loaded");
